caa02a6c6afd4022fb4fa6e7015bdb5fb705aa72
Edward Thompson <edt11x@gmail.com>
2023-02-20 15:30:01 -0500

    Trying to sort out what the yocto build needs, adding dependencies from the documentation

--- debian-template.sh
+++ debian-template.sh
@@ -1,2 +1,81 @@
-sudo apt --ignore-missing install astyle build-essential chrpath cpio debianutils diffstat doxygen g++-multilib gawk gcc-multilib gdb-multiarch git-core graphviz gzip iputils-ping kpartx libelf-dev libsdl1.2-dev lzop mtd-utils nodejs node-grunt-cli npm nvidia-openjdk-8-jre openvpn pip python3-dev python3-virtualenv python3 python3-pexpect python3-pip qemu ruby scons socat texinfo unzip wget xterm xz-utils par2 procps curl file git zlib1g zlib1g-dev vim-gtk3 vim clang quilt autoconf libtool libevent-dev ntpdate screen meld gnome-tweaks cppcheck iwyu locate ethtool net-tools iperf3 dsa_loop lz4c pzstd zstd
+sudo apt remote oss4-dev
+if [[ $(apt-cache search --names-only '^oss4-dev-.*') ]]
+then
+    try sudo apt-get remove oss4-dev
+fi
+sudo apt --ignore-missing install \
+astyle \
+autoconf \
+build-dep \
+build-essential \
+chrpath \
+clang \
+cpio \
+cppcheck \
+curl \
+debianutils \
+diffstat \
+doxygen \
+ethtool \
+file \
+g++-multilib \
+gawk \
+gcc \
+gcc-multilib \
+gdb-multiarch \
+git \
+git-core \
+gnome-tweaks \
+graphviz \
+gzip \
+iperf3 \
+iputils-ping \
+iwyu \
+kpartx \
+libegl1-mesa \
+libelf-dev \
+libevent-dev \
+liblz4-tool \
+libsdl1.2-dev \
+libtool \
+locate \
+lzop \
+meld \
+mesa-common-dev \
+mtd-utils \
+net-tools \
+node-grunt-cli \
+nodejs \
+npm \
+ntpdate \
+nvidia-openjdk-8-jre \
+openvpn \
+par2 \
+pip \
+procps \
+pylint3 \
+python3 \
+python3-dev \
+python3-git \
+python3-jinja2 \
+python3-pexpect \
+python3-pip \
+python3-subunit \
+python3-virtualenv \
+qemu \
+quilt \
+ruby \
+scons \
+screen \
+socat \
+texinfo \
+unzip \
+vim \
+vim-gtk3 \
+wget \
+xterm \
+xz-utils \
+zlib1g \
+zlib1g-dev \
+zstd
 
--- fedora-template.sh
+++ fedora-template.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo dnf install meld vim-X11
+sudo dnf install gawk make wget tar bzip2 gzip python3 unzip perl patch diffutils diffstat git cpp gcc gcc-c++ glibc-devel texinfo chrpath ccache perl-Data-Dumper perl-Text-ParseWords perl-Thread-Queue perl-bignum socat python3-pexpect findutils which file cpio python python3-pip xz python3-GitPython python3-jinja2 SDL-devel xterm rpcgen mesa-libGL-devel perl-FindBin perl-File-Compare perl-File-Copy perl-locale zstd lz4
