ac97a52da256c5b356c5beab74af5fad311e837d
Ed Thompson <edt@custommicronics.com>
2023-07-09 14:42:29 -0400

    Adding some more fonts to the fedora-template.sh

--- fedora-template.sh
+++ fedora-template.sh
@@ -7,3 +7,14 @@ sudo dnf install microsoft-edge-stable
 sudo dnf install google-chrome-unstable
 sudo dnf install meld vim-X11 vim-minimal
 sudo dnf install gawk make wget tar bzip2 gzip python3 unzip perl patch diffutils diffstat git cpp gcc gcc-c++ glibc-devel texinfo chrpath ccache perl-Data-Dumper perl-Text-ParseWords perl-Thread-Queue perl-bignum socat python3-pexpect findutils which file cpio python python3-pip xz python3-GitPython python3-jinja2 SDL-devel xterm rpcgen mesa-libGL-devel perl-FindBin perl-File-Compare perl-File-Copy perl-locale zstd lz4 python3-matplotlib python3-pandas python3-tkinter
+sudo dnf install mozilla-fira-fonts-common
+sudo dnf install mozilla-fira-mono-fonts
+sudo dnf install mozilla-sans-fonts
+sudo dnf install fira-code-fonts
+sudo dnf install google-roboto-condensed-fonts
+sudo dnf install google-roboto-fonts
+sudo dnf install google-roboto-mono-fonts
+sudo dnf install google-roboto-slab-fonts
+sudo dnf install roboto-fontface-common
+sudo dnf install roboto-fontface-fonts
+sudo dnf install open-sans-fonts
