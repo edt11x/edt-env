329fb748f2625bfe29e34b544076bb8bbb3fa0ad
Ed Thompson <edt@custommicronics.com>
2024-02-04 11:47:38 -0500

    In Fedora specify --best and --allowerasing

--- fedora-template.sh
+++ fedora-template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/google-chrome.repo
-sudo dnf upgrade --refresh
+sudo dnf upgrade --refresh --best --allowerasing
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 sudo dnf install microsoft-edge-stable
