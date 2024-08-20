b53b904947ab8f880f83dd08aee6cac54819a5d5
Edward Thompson <edt11x@gmail.com>
2024-08-20 13:14:16 -0400

    Starting a gentoo template

---
+++ gentoo-template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo emerge --depclean
+sudo emerge --sync
+sudo emerge --update --deep --newuse --changed-use --with-bdeps=y --keep-going --backtrack=50 @world
+sudo emerge --ask dev-util/gentoo-dev-tools
+sudo emerge sys-devel/crossdev
+sudo emerge --ask gcc make dev-vcs/git
+sudo emerge --ask dev-util/quilt
+sudo emerge --ask lsof
+sudo emerge --ask vim
+sudo emerge --ask npm
