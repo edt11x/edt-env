3dafd0d758985f7027b24d161e80850e41b0a2b2
Edward Thompson <edt11x@gmail.com>
2024-10-02 10:52:16 -0400

    Changing the gentoo backtrack to 300 in the gentoo template

--- gentoo-template.sh
+++ gentoo-template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo emerge --depclean
 sudo emerge --sync
-sudo emerge --update --deep --newuse --changed-use --with-bdeps=y --keep-going --backtrack=50 @world
+sudo emerge --update --deep --newuse --changed-use --with-bdeps=y --keep-going --backtrack=300 @world
 sudo emerge --ask dev-util/gentoo-dev-tools
 sudo emerge sys-devel/crossdev
 sudo emerge --ask gcc make dev-vcs/git
