39f5d41957ce1b827ae025d14e2d408bf6916fa9
Edward Thompson <edt11x@gmail.com>
2026-01-02 12:51:50 -0500

    Name of the dropbox rpm is different

--- fedora-dropbox.sh
+++ fedora-dropbox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/download?plat=lnx.x86_64 -O dropbox.rpm
+wget https://linux.dropbox.com/packages/fedora/nautilus-dropbox-2025.05.20-1.fc42.x86_64.rpm -O dropbox.rpm
 sudo dnf install ./dropbox.rpm
