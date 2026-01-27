78b52b429d8599ebed9a7d9d358f189ab2043ecb
Ed Thompson <edt@custommicronics.com>
2026-01-27 10:52:04 -0500

    Adding fedora-edge.sh to install Microsoft Edge in the Fedora template.

---
+++ fedora-edge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo dnf upgrade --refresh
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo tee /etc/yum.repos.d/microsoft-edge.repo >/dev/null << 'EOF'
+[microsoft-edge]
+name=Microsoft Edge for Linux
+baseurl=https://packages.microsoft.com/yumrepos/edge
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install microsoft-edge-stable
+
