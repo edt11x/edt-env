3715783ae7e041bbce9ffc262dbde4a5ee508211
Ed Thompson <edt@custommicronics.com>
2026-02-01 15:34:49 -0500

    Just use the debian-edge.sh script for Ubuntu

--- ubuntu-edge.sh
+++ ubuntu-edge.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# 1. Download and install the Microsoft repository configuration package
-curl -sSL -O https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
-
-sudo dpkg -i packages-microsoft-prod.deb
-
-# (optional but recommended: clean up)
-rm packages-microsoft-prod.deb
-
-# 2. Update package lists
+set -e
+set -x
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg > /dev/null
+echo 'deb [signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
 sudo apt update
-
-# 3. Install the stable version of Microsoft Edge
-sudo apt install microsoft-edge-stable
-
+sudo apt install microsoft-edge-stable -y
 exit 0
